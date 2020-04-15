0247fc838a24e770f446f0a98d5b8e7717c58b92
Teemu Desktop <teemur11@gmail.com>
2020-04-15 20:36:52 +0300

    Added current wappuradio song to status

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
+const io = require('socket.io-client');
 
 const commands = require('./commands');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+
+    const socket = io('https://wappuradio.fi/');
+    socket.on('np', (song => {
+        client.user.setPresence({ activity: { name: song.song, type: 'LISTENING', url: 'https://wappuradio.fi/' }, status: 'online' })
+            .catch(console.error);
+    }));
 });
 
 client.on('message', msg => {
