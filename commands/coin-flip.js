12435a99f7e20d0a69bacdc2ba8e195bed839de4
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-05-21 16:09:26 +0300

    Flipsstats for specific user

--- commands/coin-flip.js
+++ commands/coin-flip.js
@@ -1,83 +1,74 @@
-const Discord = require("discord.js");
-const mongoUtil = require("../mongoUtil");
+const Discord = require('discord.js')
+const mongoUtil = require('../mongoUtil')
 
-let collection = undefined;
+let collection = undefined
 let GetCollection = () => {
   if (!collection) {
-    collection = mongoUtil.getDb().collection("flipstats");
+    collection = mongoUtil.getDb().collection('flipstats')
   }
-  return collection;
-};
+  return collection
+}
 
-let coinFlips = {};
+let coinFlips = {}
 
 exports.flip = {
-  help: "Flips a coin for you",
-  usage: "!flip <heads/tails>",
-  aliases: ["f", "(╯°□°）╯︵ ┻━┻"],
+  help: 'Flips a coin for you',
+  usage: '!flip <heads/tails>',
+  aliases: ['f', '(╯°□°）╯︵ ┻━┻'],
   /**
    * Flips a coin for the user
    * @param {string[]} args - Command arguments
    * @param {Discord.Message} msg - User's message
    */
   func: (args, msg) => {
-    if (
-      coinFlips[msg.author] &&
-      new Date() - coinFlips[msg.author].time < 30 * 60 * 1000
-    ) {
-      let diff = 30 * 60 * 1000 - (new Date() - coinFlips[msg.author].time);
-      let min = Math.floor(diff / (60 * 1000));
-      let sec = Math.floor((diff - min * 60 * 1000) / 1000);
+    if (coinFlips[msg.author] && new Date() - coinFlips[msg.author].time < 30 * 60 * 1000) {
+      let diff = 30 * 60 * 1000 - (new Date() - coinFlips[msg.author].time)
+      let min = Math.floor(diff / (60 * 1000))
+      let sec = Math.floor((diff - min * 60 * 1000) / 1000)
 
       msg
         .reply(
           `You already flipped a coin and have to wait ${min} minutes and ${sec} seconds\nYou guessed **${
-            coinFlips[msg.author].guess === 0 ? "heads" : "tails"
-          }** and got **${
-            coinFlips[msg.author].result === 0 ? "heads" : "tails"
-          }**`
+            coinFlips[msg.author].guess === 0 ? 'heads' : 'tails'
+          }** and got **${coinFlips[msg.author].result === 0 ? 'heads' : 'tails'}**`
         )
-        .catch(console.error);
+        .catch(console.error)
 
-      return;
+      return
     }
 
-    if (args[0] !== undefined) args[0] = args[0].toLocaleLowerCase();
-    if (args[0] === "h" || args[0] === "0") args[0] = "heads";
-    if (args[0] === "t" || args[0] === "1") args[0] = "tails";
+    if (args[0] !== undefined) args[0] = args[0].toLocaleLowerCase()
+    if (args[0] === 'h' || args[0] === '0') args[0] = 'heads'
+    if (args[0] === 't' || args[0] === '1') args[0] = 'tails'
 
-    if (args[0] === undefined || (args[0] !== "heads" && args[0] !== "tails")) {
-      msg
-        .reply(
-          `You have to also guess **heads** or **tails**\n!flip <heads/tails>`
-        )
-        .catch(console.error);
+    if (args[0] === undefined || (args[0] !== 'heads' && args[0] !== 'tails')) {
+      msg.reply(`You have to also guess **heads** or **tails**\n!flip <heads/tails>`).catch(console.error)
 
-      return;
+      return
     }
 
-    let guess = args[0] === "heads" ? 0 : 1;
+    let guess = args[0] === 'heads' ? 0 : 1
 
     coinFlips[msg.author] = {
       time: new Date(),
       guess: guess,
       result: -1,
-    };
+    }
 
     msg
-      .reply("Flipping coin...")
+      .reply('Flipping coin...')
       .then((message) => {
         setTimeout(() => {
-          let rnd = Math.floor(Math.random() * 2);
+          let rnd = Math.floor(Math.random() * 2)
           message.edit(
-            `${msg.author}, You got **${rnd === 0 ? "heads" : "tails"}**! ${
+            `${msg.author}, You got **${rnd === 0 ? 'heads' : 'tails'}**! ${
               rnd === guess
-                ? "You can leave if you want to"
-                : "You have to spend another 30 minutes on the computer"
+                ? 'You can leave if you want to'
+                : 'You have to spend another 30 minutes on the computer'
             }`
-          );
+          )
 
-          coinFlips[msg.author].result = rnd;
+          coinFlips[msg.author].result = rnd
 
           GetCollection().findOne({ id: msg.author.id }, (err, result) => {
             if (result === null) {
@@ -87,7 +78,7 @@ exports.flip = {
                 wins: rnd === guess ? 1 : 0,
                 heads: guess === 0 ? 1 : 0,
                 tails: guess === 1 ? 1 : 0,
-              });
+              })
             } else {
               GetCollection().updateOne(
                 { id: msg.author.id },
@@ -99,45 +90,53 @@ exports.flip = {
                     tails: guess === 1 ? 1 : 0,
                   },
                 }
-              );
+              )
             }
-          });
-        }, 1000);
+          })
+        }, 1000)
       })
-      .catch(console.error);
+      .catch(console.error)
   },
-};
+}
 
 exports.flipstats = {
-  help: "Gives flips stats",
-  usage: "!flipstats [username]",
-  aliases: ["fs", "fstats", "fstatz"],
+  help: 'Gives flips stats',
+  usage: '!flipstats [username]',
+  aliases: ['fs', 'fstats', 'fstatz'],
   /**
    * Flips a coin for the user
    * @param {string[]} args - Command arguments
    * @param {Discord.Message} msg - User's message
    */
   func: (args, msg) => {
-    GetCollection()
-      .find({})
-      .toArray(async (err, result) => {
-        result.sort((a, b) => b.total - a.total);
+    let query = {}
+    if (args.length > 0) {
+      const id = args[0].match(/([0-9]{18})/g)
+      if (id) {
+        query.id = id[0]
+      }
+    }
 
-        let str = "**Flip stats:**\n";
+    GetCollection()
+      .find(query)
+      .toArray(async (err, result) => {
+        result.sort((a, b) => b.wins - a.wins)
+
+        let str = '**Flip stats:**\n'
         for (let i = 0; i < result.length; i++) {
-          const user = result[i];
-          str += `\t${i + 1}. ${msg.guild.member(user.id).displayName}: ${
-            user.total
-          } flips, ${Math.round((user.wins / user.total) * 100)}%, ${
+          const user = result[i]
+          str += `\t${i + 1}. ${msg.guild.member(user.id).displayName}: ${user.total} flips, ${Math.round(
+            (user.wins / user.total) * 100
+          )}%, ${
             user.tails < user.heads
-              ? "mains heads"
+              ? 'mains heads'
               : user.heads < user.tails
-              ? "mains tails"
-              : "indecisive cunt"
-          }\n`;
+              ? 'mains tails'
+              : 'indecisive cunt'
+          }\n`
         }
 
-        msg.channel.send(str);
-      });
+        msg.channel.send(str)
+      })
   },
-};
+}
